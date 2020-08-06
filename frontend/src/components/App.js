7936bff1163d66190e5a14e87f0ef8c9b7ca8bd1
Radu Albastroiu <albastroiuradug@gmail.com>
2020-08-07 00:01:56 +0300

    Remove unnecessary code

---
+++ frontend/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Tyre pressure Project is up and running</p>
+    </div>
+  );
+}
+
+export default App;
