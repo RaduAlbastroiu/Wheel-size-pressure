2b51b411aca103177d758a2ee549a7b27573fbdf
Radu Albastroiu <albastroiuradug@gmail.com>
2020-08-16 22:51:43 +0300

    Get years for model

--- backend/models/controller.js
+++ backend/models/controller.js
@@ -13,6 +13,15 @@ class ModelsController {
     });
     return res.data;
   }
+
+  async findModelYears(make, model) {
+    let url =
+      `https://api.wheel-size.com/v1/models/${make}/${model}/?user_key=` +
+      process.env.WHEEL_FITMENT_KEY;
+
+    let res = await axios.get(url);
+    return res.data.years;
+  }
 }
 
 module.exports = ModelsController;
