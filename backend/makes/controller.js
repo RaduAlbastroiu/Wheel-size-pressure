63f7b95101cf0da9c71078e2f5d7e19ee7a21b22
Radu Albastroiu <albastroiuradug@gmail.com>
2020-08-17 19:18:34 +0300

    Cache for makes

--- backend/makes/controller.js
+++ backend/makes/controller.js
@@ -1,14 +1,58 @@
 const axios = require('axios');
 
+const MakeModel = require('./model');
+
 class MakeController {
-  async find() {
+  async shouldUpdate() {
+    let maker = await MakeModel.findOne({});
+    return (
+      parseInt(process.env.CACHE_MAKE_TIME, 10) < Date.now() - maker.lastSync
+    );
+  }
+
+  async updateData(data) {
+    data.forEach(async (maker) => {
+      let make = new MakeModel({
+        slug: maker.slug,
+        name: maker.name,
+        name_en: maker.name_en,
+        lastSync: Date.now(),
+      });
+      await make.save();
+    });
+  }
+
+  async getApi() {
     let url =
       'https://api.wheel-size.com/v1/makes/?user_key=' +
       process.env.WHEEL_FITMENT_KEY;
 
     try {
       let res = await axios.get(url);
+      await this.updateData(res.data);
       return res.data;
+    } catch (err) {
+      console.log({ message: err.message, url: err.config.url });
+      throw 'not found';
+    }
+  }
+
+  async getDB() {
+    let res = await MakeModel.find(
+      {},
+      { _id: 0, slug: 1, name: 1, name_en: 1 }
+    );
+    return res;
+  }
+
+  async find() {
+    try {
+      let shouldUpdate = await this.shouldUpdate();
+      if (shouldUpdate) {
+        return await this.getApi();
+      } else {
+        return await this.getDB();
+      }
     } catch (err) {
       console.log({ message: err.message, url: err.config.url });
       throw 'not found';
