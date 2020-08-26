1b2cf66fa6fda6dff5ea3bacc5b00d873999ceb4
Radu Albastroiu <albastroiuradug@gmail.com>
2020-08-26 23:58:04 +0300

    Simplify frontend

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import CardTab from './CardTab';
-import FormControl from './FormControl';
 
 class Dashboard extends Component {
   render() {
@@ -9,14 +7,6 @@ class Dashboard extends Component {
         <h2 style={{ marginTop: 40, marginBottom: 40, textAlign: 'center' }}>
           Tyre pressure finder
         </h2>
-        <div
-          className="row"
-          style={{ display: 'flex', justifyContent: 'center' }}
-        >
-          <CardTab text="By Vehicle"></CardTab>
-          <CardTab text="By Tire Size"></CardTab>
-          <CardTab text="By Rim Size"></CardTab>
-        </div>
       </div>
     );
   }
